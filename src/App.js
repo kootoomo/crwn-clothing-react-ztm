4a8ead6f88d6c4cc82e41c463d19094f232ed7a4
koo <kootoomo@gmail.com>
2022-11-26 01:53:06 +0300

    add crwn-clthing reactZtM

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./components/routes/home/home.component.jsx";
+import Navigation from "./components/routes/navigation/navigation.component.jsx";
+import Authentication from "./components/routes/authentication/authentication.component";
+import Shop from "./components/routes/shop/shop.component.jsx";
+import Checkout from "./components/routes/checkout/checkout.component.jsx";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop/*" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
